a45c8259bf8198707aebfa104e4525aa40dc50da
ViggoDAtSchool <viggo.drygajlo@education.nsw.gov.au>
2024-08-23 05:24:52 -0700

    done. FINALLY

---
+++ README.txt
@@ -0,0 +1,10 @@
+Step 1:
+Press run in main.py
+Step 2:
+Input amount of videos you want included in your search results
+Step 3:
+Click link provided and sign into your Google Account
+Step 4:
+Choose one of the 5 options for what you want to do, simply input the number for the instruction
+Step 5:
+Results 8)
--- main.py
+++ main.py
@@ -5,16 +5,15 @@
 # https://developers.google.com/explorer-help/code-samples#python
 
 import os
-import json as js
 import pandas as pd
-import matplotlib as mlp
+import chartify as ch
 import google_auth_oauthlib.flow
 import googleapiclient.discovery
 import googleapiclient.errors
 
 scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
 
-def main():
+def main(z):
     # Disable OAuthlib's HTTPS verification when running locally.
     # *DO NOT* leave this option enabled in production.
     os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
@@ -31,12 +30,11 @@ def main():
         api_service_name, api_version, credentials=credentials)
 
     request = youtube.videos().list(
-        part="snippet,contentDetails,statistics",
+        part="snippet, statistics",
         chart="mostPopular",
-        maxResults=100
+        maxResults=z
         )
     global data
-    #data = []
     response = request.execute()
     its = response['items']
     cols = {}
@@ -48,9 +46,55 @@ def main():
                 cols[k] = [(None if k not in j[i] else j[i][k]) for j in its]
     global data
     data = pd.DataFrame(cols)
-    #data = pd.concat(data, ignore_index=False, sort=True)
+    for i in ['viewCount', 'likeCount']:
+        data[i] = data[i].astype('int')
+    for i in ['publishedAt', 'commentCount']:
+        data[i] = data[i].astype('object')
+    global data_clean
+    data_clean = data[['title', 'id', 'viewCount', 'likeCount', 'publishedAt', 'commentCount']]
+
+def plot(x,y):
+    global view
+    view = ch.Chart(blank_labels=True, x_axis_type='linear', y_axis_type='linear')
+    view.plot.scatter(
+        data_frame=data,
+        x_column=x,
+        y_column=y,
+    )
+    view.plot.text(
+        data_frame=data,
+        x_column=x,
+        y_column=y,
+        text_column='',
+        x_offset=1,
+        y_offset=0,
+        font_size='10pt'
+    )
+    view.set_title(x+' vs. '+y)
+    view.set_subtitle('A simple plot of '+x+' compared to '+y+'.')
+    view.show()
 
 if __name__ == "__main__":
-    main()
-    print(data)
-    data.to_csv('data.csv', index=False)
+    val=input("How many videos would you like to search for? ")
+    main(val)
+    choice=input("""What would you like to do?
+                 1: Print the original DataFrame.
+                 2: Print the cleaned DataFrame.
+                 3: Show a scatterplot of viewcount vs. likecount for the videos you searched.
+                 4: Transfer the original DataFrame to a .csv file.
+                 5: Exit the program.
+                 """)
+    if choice == "1":
+        print(data)
+    elif choice == "2":
+        print(data_clean)
+    elif choice == "3":
+        x='viewCount'
+        y='likeCount'
+        plot(x,y)
+    elif choice == "4":
+        data.to_csv('data.csv', index=False)
+    elif choice == "5":
+        pass
+    else:
+        print("That is not a valid value, dummy.")
